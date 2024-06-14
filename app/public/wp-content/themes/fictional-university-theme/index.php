c92103edac945f694629c03eb705c8a3e01094bb
Manel Castro Fonseca <manelcastrofonseca@MacBook-Pro-de-Manel.local>
2024-06-14 15:06:36 +0200

    test

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -20,7 +20,6 @@
                 the_post(); ?>
 
                 <div class="post-item"> 
-
                 <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
                 <div class="metabox">
